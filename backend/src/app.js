9bfc49ace2bd858fdafeb90e0006131ebc994136
SaptakGit <sadcat14@gmail.com>
2025-02-03 18:53:10 +0530

    EP-04: Advanced Routing with ?/+/*, Dynamic route with req.query & req.params

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,21 +2,9 @@ const express = require('express');
 
 const app = express();
 
-app.use("/test", (req, res) => {
-    res.send('Hello from Test...');
-});
-
-app.get("/user", (rreq, res) => {
+app.get("/user", (req, res) => {
     res.send({"firstname": "Saptak", "lastname" : "Das"})
 });
-
-app.post("/user", (rreq, res) => {
-    res.send("Data saved successfully to the database")
-});
-
-app.delete("/user", (req, res) => {
-    res.send("User deleted suceesfully")
-})
 
 app.listen(7777, () => {
     console.log('Server is listining and running on PORT 7777...');
