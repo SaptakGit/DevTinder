863d562302a5694ef5ddcb6f6bd4e63e8c5bb1ac
SaptakGit <sadcat14@gmail.com>
2025-02-03 18:25:42 +0530

    EP-04: Postman, exploring GET/POST/DELETE HTTP Methods

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,18 +2,22 @@ const express = require('express');
 
 const app = express();
 
-app.use("/", (req, res) => {
-    res.send('Hello from dashboard...');
-});
-
 app.use("/test", (req, res) => {
     res.send('Hello from Test...');
 });
 
-app.use("/hello", (req, res) => {
-    res.send('Hellog from Hello Page...');
+app.get("/user", (rreq, res) => {
+    res.send({"firstname": "Saptak", "lastname" : "Das"})
 });
 
-app.listen(3000, () => {
-    console.log('Server is listining and running on PORT 3000...');
+app.post("/user", (rreq, res) => {
+    res.send("Data saved successfully to the database")
+});
+
+app.delete("/user", (req, res) => {
+    res.send("User deleted suceesfully")
+})
+
+app.listen(7777, () => {
+    console.log('Server is listining and running on PORT 7777...');
 });
