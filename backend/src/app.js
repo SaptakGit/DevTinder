7e9665635029b652747a8d8862251c0ffd0d9748
SaptakGit <sadcat14@gmail.com>
2025-02-04 07:58:32 +0530

    EP-05: Error Handling, Try/Catch, Wildcard Error Handling

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,25 +2,32 @@ const express = require('express');
 
 const app = express();
 
-const {adminAuth, userAuth} = require("./middlewares/auth");
+// Using Wild Card Error Handling
+app.get("/getUserData", (req, res) => {
+    throw new Error("server not found");
+    res.send("User Data Sent");
+});
 
-app.use("/admin", adminAuth);
 
-app.get("/user/login",(req, res) => {
-    res.send("User Logged in Successfully!")
-}) // Middleware NOT Checked 
+// Using Try/Catch Error Handling
+app.get("/getUser", (req, res) => {
+    try{
+        throw new Error("server not found");
+        res.send("User Data Sent");
+    } catch(err){
+        // log the error
+        res.status(500).send("Some Error Occur");
+    }
+    
+});
 
-app.get("/user",userAuth, (req, res) => {
-    res.send("User Data Sent")
-}) // Middleware For User Checked 
-
-app.get("/admin/getAllUser", (req, res) => {
-    res.send("All Data Sent")
-}) // Middleware For Admin Checked
-
- app.get("/admin/deleteUser", (req, res) => {
-    res.send("User Deleted")
-}) // Middleware For Admin Checked
+// Keep Wildcard Error towards the End
+app.use("/", (err, req, res, next) => {
+    if(err){
+        // log the error
+        res.status(500).send("Something Went Wrong");
+    }
+});
 
 app.listen(7777, () => {
     console.log('Server is listining and running on PORT 7777...');
