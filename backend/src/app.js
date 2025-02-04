3d29f8a23537697885382658c158d3456321c7a0
SaptakGit <sadcat14@gmail.com>
2025-02-04 07:39:40 +0530

    EP-05: Response Handler, Request Handler,Create Besic Middleware & Auth

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,9 +2,25 @@ const express = require('express');
 
 const app = express();
 
-app.get("/user", (req, res) => {
-    res.send({"firstname": "Saptak", "lastname" : "Das"})
-});
+const {adminAuth, userAuth} = require("./middlewares/auth");
+
+app.use("/admin", adminAuth);
+
+app.get("/user/login",(req, res) => {
+    res.send("User Logged in Successfully!")
+}) // Middleware NOT Checked 
+
+app.get("/user",userAuth, (req, res) => {
+    res.send("User Data Sent")
+}) // Middleware For User Checked 
+
+app.get("/admin/getAllUser", (req, res) => {
+    res.send("All Data Sent")
+}) // Middleware For Admin Checked
+
+ app.get("/admin/deleteUser", (req, res) => {
+    res.send("User Deleted")
+}) // Middleware For Admin Checked
 
 app.listen(7777, () => {
     console.log('Server is listining and running on PORT 7777...');
