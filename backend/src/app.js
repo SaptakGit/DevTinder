06f26902a322659e41add48cea607dd69d2db3f1
SaptakGit <sadcat14@gmail.com>
2025-02-05 07:33:47 +0530

    EP-06: create Model, create Schema, create a POST signup API using Model

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,25 @@
 const express = require('express');
-const {connectDB} = require("./config/database");
+const connectDB = require("./config/database");
 const app = express();
+const User = require("./models/user")
+
+
+app.post("/signup", async (req,res) => {
+    const user = new User({
+        firstName: "Sachin",
+        lastName: "Tendulker",
+        emailId: "virat@gmail.com",
+        password: "Sachin@879"
+    });
+
+    try{
+        await user.save();
+        res.send("User added successfully!!");
+    } catch (err){
+        res.status(400).send("Error saving user"+ err.message);
+    }
+});
+
 
 connectDB()
     .then(() => {
