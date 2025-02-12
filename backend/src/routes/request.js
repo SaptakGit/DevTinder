add4bb6664f9d26f03e506e21670751664fe8c02
SaptakGit <sadcat14@gmail.com>
2025-02-12 07:38:22 +0530

    EP-12: Creating SendConnectionRequest API, validate the API, Create connectionRequest Schema, validate corner cases of API, schema.pre

--- backend/src/routes/request.js
+++ backend/src/routes/request.js
@@ -1,13 +1,50 @@
 const express = require("express");
 const requestRouter = express.Router();
 const { userAuth } = require("../middlewares/auth");
+const ConnectionRequest = require("../models/connectionRequest");
+const User = require("../models/user");
 
-requestRouter.get("/sendConnectionRequest", userAuth, async (req, res) => {
-    const user = req.user;
-    // Sending a connection request
-    console.log("Sending a connection request");
+requestRouter.post("/request/send/:status/:toUserId", userAuth, async (req, res) => {
+    try{
+        const fromUserId = req.user._id;
+        const toUserId = req.params.toUserId;
+        const status = req.params.status;
 
-    res.send(user.firstName + "Send the connect request");
+        const allowedStatus = ["ignored", "interested"];
+        if(!allowedStatus.includes(status)){
+            return res.status(400).json({message: "Invalid status type: " + status});
+        }
+
+        // Check for the valid user id
+        const toUser = await User.fndById(toUserId);
+        if(!toUser){
+            return res.status(404).json({message : "User not found"});
+        }
+
+        // If there is an existing connection request
+        const existingConnectionRequest = await ConnectionRequest.findOne({
+            $or: [
+                { fromUserId, toUserId },
+                { fromUserId : toUserId, toUserId : fromUserId }
+            ],
+        });
+        if(existingConnectionRequest){
+            return res.status(400).send({message : "Connection Request already Exists"});
+        }
+
+        const connectionRequest = new ConnectionRequest({
+            fromUserId, toUserId, status
+        });
+
+        const data = await connectionRequest.save();
+        res.json({
+            message: "Connection Request Sent Successfullt!",
+            data,
+        });
+
+    } catch(err){
+        res.status(400).send("ERROR: " + err.message);
+    }
 })
 
 module.exports = requestRouter;
