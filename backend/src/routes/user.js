85b0e77a780fd288db727446c4f0a7a4c4e3c933
SaptakGit <sadcat14@gmail.com>
2025-02-15 08:49:40 +0530

    EP-14: Creating feed API, .select(), , , Using Set Data Structure

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -2,6 +2,7 @@ const express = require("express")
 const userRouter = express.Router();
 const { userAuth } = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequest");
+const User = require("../models/user");
 
 const USER_SAFE_DATA = "firstName lastName photoUrl age about gender skills";
 
@@ -59,6 +60,41 @@ userRouter.get("/user/connecetions", userAuth, async (req, res)=>{
 
     } catch (err){
         res.status(400).send("ERROR: " + err.message);
+    }
+})
+
+// Get feed *** VVI ***
+userRouter.get("/feed", userAuth, async (req, res) => {
+    try{
+
+        const loggedInUser = req.user;
+
+        const connectionRequests = await ConnectionRequest.find({
+            $or:[
+                {fromUserId: loggedInUser._id},
+                {toUserId: loggedInUser._id}
+            ],
+        }).select("fromUserId toUserId");
+
+        const hideUserFromFeed = new Set();
+        connectionRequests.forEach((req) => {
+            hideUserFromFeed.add(req.fromUserId);
+            hideUserFromFeed.add(req.toUserId)
+        })
+
+        //console.log(hideUserFromFeed);
+
+        const user = await User.find({
+            $and:[
+                { _id: {$nin: Array.from(hideUserFromFeed)} },
+                { _id: {$ne: loggedInUser._id}}
+            ]
+        }).select(USER_SAFE_DATA);
+
+        res.send(user);
+
+    } catch(err){
+        res.status(400).json({message : err.message});
     }
 })
 
