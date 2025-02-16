d0d2fd6b6791c15629af31857d20aaa87795124b
SaptakGit <sadcat14@gmail.com>
2025-02-17 00:48:19 +0530

    EP-17: Create Feed Feature, Feed Card, Feed Slice, Create & Update Profile, Use Toast for Notification

--- frontend/src/components/Feed.jsx
+++ frontend/src/components/Feed.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { BASE_URL } from '../utlis/constants'
+import { useDispatch, useSelector } from 'react-redux'
+import { addFeed } from '../utlis/feedSlice';
+import axios from 'axios';
+import UserCard from './UserCard';
 
 const Feed = () => {
+  const feed = useSelector((store) => store.feed)
+  const dispath = useDispatch();
+
+  const getFeed = async () => {
+    if (feed) return;
+    try {
+      const res = await axios.get(BASE_URL + "/feed", {withCredentials:true});
+      dispath(addFeed(res?.data?.data));
+    } catch(err){
+      console.log(err.message);
+    }
+  };
+
+useEffect(() =>{
+  getFeed();
+}, []);
+
   return (
-    <div>Feed</div>
+    feed && (<div className='flex justify-center my-10'>
+      <UserCard user={feed[0]}/>
+    </div>)
   )
 }
 
