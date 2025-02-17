682c8ed66eb3fa7da6025e36143e74fe572b7019
SaptakGit <sadcat14@gmail.com>
2025-02-17 23:36:00 +0530

    EP-18: Create Accept/Reject Buttons on Connection Page, Make Accept/Reject Functionality, add removeRequest reducer to requestSlice

--- frontend/src/components/Requests.jsx
+++ frontend/src/components/Requests.jsx
@@ -1,12 +1,25 @@
 import axios from 'axios'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { BASE_URL } from '../utlis/constants'
 import { useDispatch, useSelector } from 'react-redux'
-import { addRequests } from '../utlis/requestSlice'
+import { addRequests, removeRequest } from '../utlis/requestSlice'
 
 const Requests = () => {
     const requests = useSelector((store) => store.requests);
     const dispatch = useDispatch();
+
+    const reviewRequest = async (status, _id) => {
+        try{
+            const res = await axios.post(
+                BASE_URL + "/request/review/" + status + "/" + _id, 
+                {}, 
+                {withCredentials:true}
+            );
+            dispatch(removeRequest(_id));
+        } catch(err){
+            console.log(err);
+        }
+    }
 
 
     const fetchRequests = async () => {
@@ -25,7 +38,7 @@ const Requests = () => {
 
     if(!requests) return;
 
-    if(requests.length === 0) return <h1>No Requests Found</h1>;
+    if(requests.length === 0) return <h1 className='flex justify-center my-10'>No Requests Found</h1>;
 
     return (
     <div className='text-center my-10'>
@@ -36,7 +49,7 @@ const Requests = () => {
 
             
             return (
-            <div className='flex m-4 p-4 rounded-lg bg-base-300 w-1/2 mx-auto' key={_id}>
+            <div className='flex justify-between items-center m-4 p-4 rounded-lg bg-base-300 w-2/3 mx-auto' key={_id}>
                 <div>
                     <img alt="user-photo" className="w-20 h-20 rounded-full" src={photoUrl}/>
                 </div>
@@ -44,6 +57,10 @@ const Requests = () => {
                     <h2 className='font-bold text-xl'>{firstName +" "+ lastName}</h2>
                     {age && gender && <p>{age + ", " + gender}</p>}
                     <p>{about}</p>
+                </div>
+                <div>
+                    <button className="btn btn-primary mx-2" onClick={() => reviewRequest('rejected', request._id)}>Reject</button>
+                    <button className="btn btn-secondary mx-2" onClick={() => reviewRequest('accepted', request._id)}>Accept</button>
                 </div>
             </div>
         )})}
