1976c68a773d5ca7e571c5e56e3680cac3a33441
SaptakGit <sadcat14@gmail.com>
2025-05-01 20:56:03 +0530

    some changes

--- admin_backend/src/app.js
+++ admin_backend/src/app.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 
 // Setting Cors and Whitelisting the Domain Name for set coockie
 app.use(cors({
-    origin: "http://localhost:5175",
+    origin: "http://localhost:5173",
     credentials: true,
 }));
 app.use(express.json());
